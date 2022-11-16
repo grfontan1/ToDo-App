ba9bddf735aa7707b4c1307385772ec74a963ae8
grfontan1 <grfontanesi@gmail.com>
2022-11-16 11:51:56 -0500

    Starting point for my first real world application These are just the bulding blocks and UI for my app. Interactivity to come.

---
+++ ToDo-App/app.js
@@ -0,0 +1,5 @@
+const btnCreate = document.getElementsByClassName('btn-main');
+const btnHide = document.getElementsByClassName('btn-toggle');
+const btnRemove = document.getElementsByClassName('btn-remove');
+
+const todos = document.getElementsByTagName('li');

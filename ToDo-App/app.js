b81b64f284879a4ab0099c5685dc5a2eb5172249
grfontan1 <grfontanesi@gmail.com>
2022-11-30 16:32:35 -0500

    An update to make the app interactive added some code to make some of the buttons interactive.

--- ToDo-App/app.js
+++ ToDo-App/app.js
@@ -1,5 +1,31 @@
-const btnCreate = document.getElementsByClassName('btn-main');
-const btnHide = document.getElementsByClassName('btn-toggle');
-const btnRemove = document.getElementsByClassName('btn-remove');
+let btnCreate = document.getElementById('btn-main');
+let btnHide = document.getElementById('btn-toggle');
+let btnRemove = document.getElementById('btn-remove');
+let todos = document.getElementsByTagName('li');
 
-const todos = document.getElementsByTagName('li');
+btnCreate.addEventListener('click', () => {
+    const newTask = document.getElementById('main')
+    const list = document.querySelector('ul')
+    const task = document.createElement('li');
+    
+    newTask.textContent = newTask.value;
+    newTask.value = '';
+    list.appendChild(task);
+})
+
+
+
+btnHide.addEventListener('click', () => {
+    const contanier = document.querySelector('.list-container');
+    if (contanier.style.display === 'none'){
+        contanier.style.display = 'block';
+    } else {
+        contanier.style.display = 'none';
+    }
+    
+})
+
+
+
+
+
